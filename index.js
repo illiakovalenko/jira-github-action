3181f1dd2c3a9571c26aaea3134b343011999f39
illiakovalenko <illia.kovalenko@sitecore.com>
2023-07-25 08:15:42 +0300

    update

--- index.js
+++ index.js
@@ -3,13 +3,18 @@
 const github = process.env.GITHUB;
 
 const octokit = new Octokit({
-  auth: process.env.GITHUB_TOKEN
+  auth: process.env.GITHUB_TOKEN,
 })
 
-const res = await octokit.request('GET /repos/{owner}/{repo}/collaborators/{username}', {
-  owner: 'Sitecore',
-  repo: 'jss',
-  username: github.event.issue.user.login,
-})
+(async () => {
+  const res = await octokit.request(
+    "GET /repos/{owner}/{repo}/collaborators/{username}",
+    {
+      owner: "Sitecore",
+      repo: "jss",
+      username: github.event.issue.user.login,
+    }
+  );
 
-console.log(res);
+  console.log(res);
+})();
