7565747b75d2fb17b43456de54a4648b5fc9200f
illiakovalenko <illia.kovalenko@sitecore.com>
2023-07-25 08:40:09 +0300

    test

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-// console.log('test');
-// console.log(JSON.stringify(process.env.GITHUB, null, 2));
 const github = JSON.parse(process.env.GITHUB);
 
-// const octokit = new Octokit({
-//   auth: process.env.GITHUB_TOKEN,
-// })
-
 (async () => {
-  console.log(github.event.issue.user.login, github.token, process.env.GITHUB_TOKEN);
+  const event = github.event.issue || github.event.pull_request;
 
-  const res = await fetch(`https://api.github.com/repos/Sitecore/jss/collaborators/${github.event.issue.user.login}`, {
-    headers: {
-      Authorization: `Bearer ${github.token}`
+  const res = await fetch(
+    `https://api.github.com/repos/${github.event.repository.owner.login}/${github.event.repository.name}/collaborators/${event.user.login}`,
+    {
+      headers: {
+        Authorization: `Bearer ${github.token}`,
+      },
     }
-  })
+  );
 
+  const isUserCollaborator = res.status === 204;
+
+  // don't create Jira issue if PR is created by collaborator
+  // if (github.event.pull_request && isUserCollaborator) return;
+
+  console.log('IS ISSUE', !!github.event.issue);
+  console.log('IS PR', !!github.event.pull_request);
+  console.log('USER', event.user.login);
+  console.log('IS COLLABORATOR', isUserCollaborator);
   console.log(res);
 })();
